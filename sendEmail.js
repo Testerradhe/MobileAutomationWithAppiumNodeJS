a1916336479601a4ad238b4f93b3fbaafb3ff478
Testerradhe <testerradhe@gmail.com>
2025-02-12 19:30:48 +0530

    New push

--- sendEmail.js
+++ sendEmail.js
@@ -4,14 +4,14 @@ async function sendReportEmail() {
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'mukesh.agrisync@gmail.com',  // Replace with your email
-            pass: 'game rmhj ywln gztl'  // Use an app password if 2FA is enabled
+            user: 'Abc@gmail.com',  // Replace with your email
+            pass: 'xyz'  // Use an app password if 2FA is enabled
         }
     });
 
     let mailOptions = {
-        from: 'mukesh.agrisync@gmail.com',
-        to: 'testermukesh@yopmail.com', // Replace with recipient email
+        from: 'abc.com',
+        to: 'XYZ@gmail.com', // Replace with recipient email
         subject: 'Test Automation Report',
         text: 'Hello,\n\nPlease find the attached test execution report.\n\nRegards,\nQA Team',
         attachments: [
