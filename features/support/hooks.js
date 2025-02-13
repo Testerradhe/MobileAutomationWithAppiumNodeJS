5683e480a4da28a5114b9ed16a933babb68f3976
Testerradhe <testerradhe@gmail.com>
2025-02-13 21:57:00 +0530

    new commit

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,11 +1,16 @@
 'use strict';
 
+// Load environment variables from .env file
+require('dotenv').config();
+
 const { Builder } = require("selenium-webdriver");
 const { Before, After } = require("@cucumber/cucumber");
 
 var createBrowserStackSession = function () {
-  return new Builder().build();
-}
+  return new Builder()
+    .usingServer(`https://${process.env.BROWSERSTACK_USERNAME}:${process.env.BROWSERSTACK_ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub`)
+    .build();
+};
 
 Before(function (scenario, callback) {
   var world = this;
